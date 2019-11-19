b83bb58a8ae018d2ea2b218d6cdc8d8b2d151b97
baicheng <2909023946@qq.com>
2019-11-19 14:38:56 +0800

    init

---
+++ src/main/java/com/baicheng/classloaderdemo/ClassloaderDemoApplication.java
@@ -0,0 +1,13 @@
+package com.baicheng.classloaderdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassloaderDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassloaderDemoApplication.class, args);
+	}
+
+}
